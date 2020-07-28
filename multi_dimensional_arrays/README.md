26908797347fb6c8d04c4aeafc41a8f44bcc87e8
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-07-27 23:15:55 -0700

    Update Rotate Image (#89)

--- multi_dimensional_arrays/README.md
+++ multi_dimensional_arrays/README.md
@@ -112,27 +112,26 @@ Output:
 ```     
 
 ```python
-def rotate_image_90_degrees(a: [[int]]) -> [[int]]:
-    size = len(a)
+def rotate_image_90_degrees(matrix: [[int]]) -> [[int]]:
+    size = len(matrix)
     layer_count = int(size / 2)
 
     for layer in range(0, layer_count):
-        first = layer
-        last = size - first - 1
+        last = size - layer - 1
 
-        for element in range(first, last):
-            offset = element - first
+        for element in range(layer, last):
+            offset = element - layer
 
-            top = a[first][element]
-            right_side = a[element][last]
-            bottom = a[last][last - offset]
-            left_side = a[last - offset][first]
+            top = matrix[layer][element]
+            right_side = matrix[element][last]
+            bottom = matrix[last][last - offset]
+            left_side = matrix[last - offset][layer]
 
-            a[first][element] = left_side
-            a[element][last] = top
-            a[last][last - offset] = right_side
-            a[last - offset][first] = bottom
-    return a
+            matrix[layer][element] = left_side
+            matrix[element][last] = top
+            matrix[last][last - offset] = right_side
+            matrix[last - offset][layer] = bottom
+    return matrix
 ```
 
 ## Spiral Matrix
--- multi_dimensional_arrays/rotate_image.py
+++ multi_dimensional_arrays/rotate_image.py
@@ -1,35 +1,22 @@
-# Note: Try to solve this task in-place (with O(1) additional memory), since this is what you'll be asked to do during
-# an interview.
-#
-# You are given an n x n 2D matrix that represents an image. Rotate the image by 90 degrees (clockwise).
-# a = [[1, 2, 3],
-#      [4, 5, 6],
-#      [7, 8, 9]]
-
-# rotateImage(a) =
-#    [[7, 4, 1],
-#     [8, 5, 2],
-#     [9, 6, 3]]
 
 
-def rotate_image_90_degrees(arr: [[int]]) -> [[int]]:
-    size = len(arr)
+def rotate_image_90_degrees(matrix: [[int]]) -> [[int]]:
+    size = len(matrix)
     layer_count = int(size / 2)
 
     for layer in range(0, layer_count):
-        first = layer
-        last = size - first - 1
+        last = size - layer - 1
 
-        for element in range(first, last):
-            offset = element - first
+        for element in range(layer, last):
+            offset = element - layer
 
-            top = arr[first][element]
-            right_side = arr[element][last]
-            bottom = arr[last][last - offset]
-            left_side = arr[last - offset][first]
+            top = matrix[layer][element]
+            right_side = matrix[element][last]
+            bottom = matrix[last][last - offset]
+            left_side = matrix[last - offset][layer]
 
-            arr[first][element] = left_side
-            arr[element][last] = top
-            arr[last][last - offset] = right_side
-            arr[last - offset][first] = bottom
-    return arr
+            matrix[layer][element] = left_side
+            matrix[element][last] = top
+            matrix[last][last - offset] = right_side
+            matrix[last - offset][layer] = bottom
+    return matrix
